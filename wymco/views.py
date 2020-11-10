0b6bff57db2ece282f777926792c880cdc5ab99d
enriqueuz <enrique.uzcategui@outlook.com>
2020-11-10 18:00:58 -0400

    Services page created

--- wymco/views.py
+++ wymco/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class ServicesView(TemplateView):
+    template_name = 'services.html'
