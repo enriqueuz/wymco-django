c863e3141afc8711153fc232d0e6da20135aea1e
enriqueuz <enrique.uzcategui@outlook.com>
2020-12-13 09:46:47 -0400

    Full responsiveness added, frontend polished

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 ''' Blog views '''
-
+from datetime import date, timedelta
 # Django
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
@@ -7,16 +7,21 @@ from django.views.generic import ListView, DetailView
 # Models
 from blog.models import BlogPost
 
-class PostList(ListView):
-    ''' Return all pulblished posts. '''
-    queryset = BlogPost.objects.filter(status=1).order_by('-created_on')
-
-class PostDetail(DetailView):
-    ''' Return post detail. '''
-    model = BlogPost
-    
+class BlogPostsSidebarMixin(object):
+    ''' Return recently published posts for sidebar '''
     def get_context_data(self, **kwargs):
         # Return latest post for sidebar
+        today = date.today()
         context = super().get_context_data(**kwargs)
-        context['blogpost_list'] = BlogPost.objects.filter(status=1).order_by('-created_on')
+        context['blogpost_list_sidebar_news'] = BlogPost.objects.filter(status=1, created_on__gte=today-timedelta(days=3)).order_by('-created_on')
+        context['blogpost_list_sidebar'] = BlogPost.objects.filter(status=1, created_on__lte=today-timedelta(days=3)).order_by('-created_on')[:3]
         return context
+
+class PostList(BlogPostsSidebarMixin, ListView):
+    ''' Return all published posts. '''
+    queryset = BlogPost.objects.filter(status=1).order_by('-created_on')
+    paginate_by = 5
+
+class PostDetail(BlogPostsSidebarMixin, DetailView):
+    ''' Return post detail. '''
+    model = BlogPost
